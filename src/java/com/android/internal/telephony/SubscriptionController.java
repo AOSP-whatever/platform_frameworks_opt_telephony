34f0e978b06a8d2f7d6b1f86929c961f68f2497a
Michael Groover <mpgroover@google.com>
2020-03-20 13:25:06 -0700

    Guard ICC ID card string behind new identifier access requirements
    In Android 10 access to device identifiers was limited to apps with
    the READ_PRIVILEGED_PHONE_STATE permission, carrier privileges, the
    READ_DEVICE_IDENTIFIERS appop set to allow, or those that pass a
    device / profile owner check. TelephonyManager#getSimSerialNumber
    was guarded behind these new access requirements, but the same value
    is still accessible via SubscriptionInfo#getCardString. While this
    API is hidden toString or a parcelable can be used to obtain it. This
    change clears out the card string in any returned SubscriptionInfo
    objects if the caller does not meet the new identifier access
    requirements.
    
    Bug: 152057778
    Bug: 173421434
    Test: atest SubscriptionControllerTest
    Change-Id: I4406f1a2859f8ab691bb9f07da6940fb85e5cbb4
    Merged-In: I4406f1a2859f8ab691bb9f07da6940fb85e5cbb4
    (cherry picked from commit 7dfee2e7c3f762d069b8c606f8591498f109ff3e)

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -3682,6 +3682,7 @@ public class SubscriptionController extends ISub.Stub {
         if (!hasSubscriberIdentifierAccess(subInfo.getSubscriptionId(), callingPackage, message)) {
             result = new SubscriptionInfo(subInfo);
             result.clearIccId();
+            result.clearCardString();
         }
         return result;
     }
